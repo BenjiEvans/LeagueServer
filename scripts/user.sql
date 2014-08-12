30709474b31ee308717778331c4a27b3c5f20089
TeamAwsome <benjievans25@gmail.com>
2014-08-12 11:36:20 -0700

    Editted sql init script

--- scripts/user.sql
+++ scripts/user.sql
@@ -1,20 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 12, 2014 at 06:09 PM
--- Server version: 5.6.17
--- PHP Version: 5.4.12
+DROP DATABASE leagueServer;
+CREATE DATABASE leagueServer;
+Use leagueServer;
+DROP TABLE if exists user;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `leagueServer`
@@ -35,6 +27,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='A simple user model' AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
