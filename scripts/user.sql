c3f194deda9a8ff3f888e163f9969a03d7ebe53a
ankitagarwal299 <ankit.agarwal299@gmail.com>
2014-08-12 12:01:02 -0700

    user script

--- scripts/user.sql
+++ scripts/user.sql
@@ -1,15 +1,23 @@
-DROP DATABASE leagueServer;
-CREATE DATABASE leagueServer;
-Use leagueServer;
-DROP TABLE if exists user;
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 12, 2014 at 06:58 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `leagueServer`
+-- Database: `leagueserver`
 --
 
 -- --------------------------------------------------------
@@ -20,14 +28,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `user` (
   `Id` int(45) NOT NULL AUTO_INCREMENT COMMENT '@Primary key {int} Id',
-  `ign` varchar(34) NOT NULL COMMENT '@property {String} ign (in game name)',
   `wins` int(10) NOT NULL COMMENT '@property {int} wins',
   `losses` int(11) NOT NULL COMMENT '@property {int} losses',
   `team` varchar(34) DEFAULT NULL COMMENT ' @property {String} team (can be null)',
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='A simple user model' AUTO_INCREMENT=1 ;
+  `password` varchar(10) NOT NULL COMMENT '@property {String} password',
+  `email` varchar(15) NOT NULL COMMENT '@property {String} email',
+  `ign` varchar(34) NOT NULL COMMENT '''@property {String} ign (in game name)''',
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `ign` (`ign`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='A simple user model' AUTO_INCREMENT=2 ;
 
-Insert Into user Values(1,'root','5','1', NULL);
+--
+-- Dumping data for table `user`
+--
 
+INSERT INTO `user` (`Id`, `wins`, `losses`, `team`, `password`, `email`, `ign`) VALUES
+(1, 5, 1, NULL, '', '', '');
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
