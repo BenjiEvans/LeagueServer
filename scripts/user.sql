342614318c0d2e6be1afe938a1ad0af182ea1ff5
ankitagarwal299 <ankit.agarwal299@gmail.com>
2014-08-12 11:13:01 -0700

    sql script for user model

---
+++ scripts/user.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 12, 2014 at 06:09 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `leagueServer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `Id` int(45) NOT NULL AUTO_INCREMENT COMMENT '@Primary key {int} Id',
+  `ign` varchar(34) NOT NULL COMMENT '@property {String} ign (in game name)',
+  `wins` int(10) NOT NULL COMMENT '@property {int} wins',
+  `losses` int(11) NOT NULL COMMENT '@property {int} losses',
+  `team` varchar(34) DEFAULT NULL COMMENT ' @property {String} team (can be null)',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='A simple user model' AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
