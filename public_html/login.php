5f61ae264fea80e76dc8afdf57b8dbbe768f6825
TeamAwsome <benjievans25@gmail.com>
2014-08-15 21:11:02 -0700

    Implemented registration controller

--- public_html/login.php
+++ public_html/login.php
@@ -1,26 +1,7 @@
-<?php
-session_start();
-//establishing database connection
-	$db = "leagueserver";
-	
-	$conn = mysql_connect("localhost","LeagueAdmin","password");
-	
-	if($conn)
-		{
-			mysql_select_db($db,$conn);
-		}
-		else
-		{
-			die('Unable to connect the database'.mysql_Error());
-		}
-?>
-
-
-
+<?php session_start();?>
+<?php require("../templates/mysql_connect.php")?>
 
 <?php
-
- 
  //get the user's submitted json 
  $json = file_get_contents('php://input');
  $obj = json_decode($json);
@@ -33,12 +14,12 @@ print $obj->{'pass'};
 $emailJSON = $obj->{'email'};
 $passJSON = $obj->{'pass'};
 
-           if(!isset($emailJSON) || !isset($passJSON)){
+       if(!isset($emailJSON) || !isset($passJSON)){
 	
-	       header("HTTP/1.0 406 Not Acceptable");
-	       return;
+	    header("HTTP/1.0 406 Not Acceptable");
+	    return;
 	
-            }
+        }
 
 	$queryTodb = mysql_query("select * from user where email ='".mysql_real_escape_string($emailJSON)."'");
 	
@@ -51,24 +32,25 @@ $passJSON = $obj->{'pass'};
 	}
 	else
 	{	
-		if($count > 1){
+	
+	    if($count > 1)
+	    {
 		header("HTTP/1.0 500 Internal Server Error");
-		return;}
+		return;
+	    }
 		
-		$row = mysql_fetch_array($queryTodb);
+	    $row = mysql_fetch_array($queryTodb);
 		
-		if($row['password'] == $passJSON)    //compare both password one from HTML page and other from fetched records from db
-		{
-			  header("HTTP/1.0 202 Accepted");
+            if($row['password'] == $passJSON)    //compare both password one from HTML page and other from fetched records from db
+	    {
+			//should actually redirect to user panel view 
+			 header("HTTP/1.0 202 Accepted");
 			 return;
 
-		}
-		else
-		{
+	     }else{
 			header("HTTP/1.0 401 Unauthorized");
 				return;
-		}
-		
+	     }
 	}
 
 
---
+++ public_html/register.php
@@ -0,0 +1,43 @@
+<?php require("../templates/mysql_connect.php") ?>
+<?php
+  
+
+
+//get the user's submitted json 
+ $json = file_get_contents('php://input');
+ $obj = json_decode($json);
+
+
+ 
+//check feilds for emptyness 
+$email= $obj->{'email'};
+$pass = $obj->{'pass'};
+$ign = $obj->{'ign'};
+
+
+       if(!isset($email) || !isset($pass) || !isset($ign)){
+	
+	    header("HTTP/1.0 406 Not Acceptable");
+	    return;
+	
+        }
+     
+        
+        $email = mysql_real_escape_string($email);
+        $pass = mysql_real_escape_string($pass);
+        $ign = mysql_real_escape_string($ign);
+        
+        //add the registering user 
+        
+           $q = mysql_query("insert into user (email,password,ign) values('$email','$pass','$ign')");
+	
+      //$q = mysql_query("select * from user");
+      
+      if($q === false){
+      	   print mysql_error();
+      	   
+	    header("HTTP/1.0 406 Not Acceptable");
+	}
+        
+       
+?>
