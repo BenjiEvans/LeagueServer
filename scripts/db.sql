c467712a70e1bb551b023e9ce6fdf33d4fc9e4e8
TeamAwsome <benjievans25@gmail.com>
2014-08-20 23:21:57 -0700

    add mysql directory for sql scripts

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,20 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 12, 2014 at 06:58 PM
--- Server version: 5.6.17
--- PHP Version: 5.4.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 DROP DATABASE leagueserver;
 CREATE DATABASE leagueserver;
