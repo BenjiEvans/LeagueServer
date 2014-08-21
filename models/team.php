3d0ec547bfe20c6acb27f9362992ba14d9160fff
ankitagarwal299 <ankit.agarwal299@gmail.com>
2014-08-21 16:53:20 -0700

    Updated Team Model Class

--- models/team.php
+++ models/team.php
@@ -1,6 +1,4 @@
-<?php session_start();?>
-<?php require("../templates/mysql_connect.php")?>
-<?php require("../templates/json_functions.php")?>
+
 
       <?php
         // The code below creates the class
@@ -10,21 +8,17 @@
             public $win;
             public $losses;
 			public $captain;
-			public $status={'Collegiate','Challenger'};
-			public $rank ;
             
             // Assigning the values
             public function __construct($id,$win,$losses,$captain, $status, $rank) {
-              $this->id = $key;              
+              $this->id = $id;              
               $this->win = $win;
 			  $this->losses = $losses;
 			  $this->captain = $captain;
-			  $this->status = $status;
-			  $this->rank = $rank;
+			  
+			  
             }
           }
           
-        // Creating a new user 
-        $team = new Team('boring', '12345', 12345);
-		
+        
         ?>
