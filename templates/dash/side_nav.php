07be45a6fa8cd0f55247c25fe3079aa8fb8cd48f
TeamAwsome <benjievans25@gmail.com>
2014-08-26 22:05:04 -0700

    Added controll pannel for user's that are root or admin status

--- templates/dash/side_nav.php
+++ templates/dash/side_nav.php
@@ -4,6 +4,10 @@
      <li id='blog' class="dash_link"><a href="#">Blog</a></li>
      <li id='team_rank' class="dash_link"><a href="#">Team Rank</a></li>
      <li id='event' class="dash_link"><a href="#">Events <span class='badge'> display # of posted events</span></a></li>
-     <li id='control_panel' class="dash_link"><a href="#">Control Panel</a></li>
+     <?php 
+       $status =$_SESSION["user"]->status();
+     if(strcmp($status,"Root") == 0 || strcmp($status,"Admin") == 0) echo "<li id='control_panel' class='dash_link'><a href='#'>Control Panel</a></li>";
+     	     
+     	    ?>
      </ul>
 </div>
